026fb31c8ed4f8c8b6fc2b6101c3b28e7f0a9f16
Pascal Ribas <84874369+PascalRibas@users.noreply.github.com>
2024-06-14 11:06:49 +0200

    update Scripts/ProcessHandling/SpecificProcesses/Combat.cs

--- Scripts/ProcessHandling/SpecificProcesses/Combat.cs
+++ Scripts/ProcessHandling/SpecificProcesses/Combat.cs
@@ -1,5 +1,9 @@
 //a collection of methods revolving around the combat step
 public class Combat
 {
-  
+  //TODO: move to a more appropriately named location
+  public static void Destroy(Card card)
+  {
+    
+  }
 }
