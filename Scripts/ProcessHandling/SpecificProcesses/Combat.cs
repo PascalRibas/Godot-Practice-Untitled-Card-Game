ad79d7e6c0f44b8eb2cce631fa4d72042c4c54d3
Pascal Ribas <84874369+PascalRibas@users.noreply.github.com>
2024-06-14 11:13:28 +0200

    update Scripts/ProcessHandling/SpecificProcesses/Combat.cs

--- Scripts/ProcessHandling/SpecificProcesses/Combat.cs
+++ Scripts/ProcessHandling/SpecificProcesses/Combat.cs
@@ -2,9 +2,23 @@
 public class Combat
 {
   //TODO: move to a more appropriately named location
-  public static void Destroy(Card card)
+  public static void Destroy(Creature creature)
   {
-    CardLocation Pile = card.GetOwner().GetDiscardPile();
-    ProcessHandling.MoveCards.MoveCard(card, Pile);
+    CardLocation Pile = creature.GetOwner().GetDiscardPile();
+    ProcessHandling.MoveCards.MoveCard(creature, Pile);
+  }
+  
+  public static void Battle (Creature attacker, Creature defender)
+  {
+    int attack = attacker.GetPower();
+    int defense = defender.GetPower();
+    if(attack >= defense)
+    {
+      Destroy(defender);
+    }
+    if(defense >= attack)
+    {
+      Destroy(attacker);
+    }
   }
 }
