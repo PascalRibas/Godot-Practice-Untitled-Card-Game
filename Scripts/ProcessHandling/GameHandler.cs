ca680cf6a3c32310dec169b660f6af2eeb2bccd4
Pascal Ribas <84874369+PascalRibas@users.noreply.github.com>
2024-06-14 12:56:05 +0200

    update Scripts/ProcessHandling/GameHandler.cs

--- Scripts/ProcessHandling/GameHandler.cs
+++ Scripts/ProcessHandling/GameHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UntitledCardGame.Scripts.ProcessHandling
+namespace ProcessHandling
 {
     public class GameHandler
     {
